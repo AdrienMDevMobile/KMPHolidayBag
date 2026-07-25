74f3080d0bf8d49b5f24b8cab64477d6516999e5
Adrien <developpeuradrien@gmail.com>
2026-07-25 08:38:39 +0200

    Define my needs in Static data

--- composeApp/src/commonMain/kotlin/m/adrien/kmpholiday/data/impl/StaticDatas.kt
+++ composeApp/src/commonMain/kotlin/m/adrien/kmpholiday/data/impl/StaticDatas.kt
@@ -2,6 +2,7 @@ package m.adrien.kmpholiday.data.impl
 
 import m.adrien.kmpholiday.data.HolidayReminderFrameworkData
 import m.adrien.kmpholiday.data.ItemInBagData
+import m.adrien.kmpholiday.view.holidayBag.component.ItemInBag
 
 internal object StaticDatas {
     val listOfHolidayBagReminder = listOf(
@@ -16,11 +17,83 @@ internal object StaticDatas {
                     quantity = 1,
                 ),
                 ItemInBagData(
+                    name = "Serviette",
+                    id = "16",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Maillot de bain",
+                    id = "15",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Tongs",
+                    id = "111",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Casquette",
+                    id = "110",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
                     name = "T shirt",
                     id = "12",
                     isDayDependant = true,
                     quantity = 1,
-                )
+                ),
+                ItemInBagData(
+                    name = "Slips",
+                    id = "13",
+                    isDayDependant = true,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Chaussettes",
+                    id = "14",
+                    isDayDependant = true,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Short",
+                    id = "17",
+                    isDayDependant = false,
+                    quantity = 2,
+                ),
+                ItemInBagData(
+                    name = "Pantalon",
+                    id = "18",
+                    isDayDependant = false,
+                    quantity = 2,
+                ),
+                ItemInBagData(
+                    name = "Affaire de sport",
+                    id = "19",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Chaussure supplementaire",
+                    id = "113",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Gourde",
+                    id = "112",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
+                ItemInBagData(
+                    name = "Belle tenue",
+                    id = "115",
+                    isDayDependant = false,
+                    quantity = 1,
+                ),
             )
         ),
         HolidayReminderFrameworkData(
